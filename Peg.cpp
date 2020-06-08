05e2de6aee06df260cbc537a465e8f70e1d458ed
Michael Dilla <michaeldilla@Michaels-MacBook-Pro.local>
2020-06-08 16:17:28 -0400

    for eclipse

--- Peg.cpp
+++ Peg.cpp
@@ -3,3 +3,11 @@
 #include <iostream>
 
 using namespace std;
+
+void Peg::changePeg() { // if has peg is true change to false adn vis-versa
+  this.hasPeg = 0 - (hasPeg - 1);
+}
+
+bool Peg::getPeg() {
+  return this.hasPeg;
+}
---
+++ Peg.h
@@ -0,0 +1,14 @@
+#ifndef PEG_H
+#define PEG_H
+
+class Peg {
+private:
+  bool hasPeg;
+  int name;
+
+public:
+  void changePeg();
+  bool getPeg();
+};
+
+#endif
